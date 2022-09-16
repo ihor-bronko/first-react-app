abc81ade5fbe99ffdc9fed0743bbb233b8e3cd46
Suric1 <igorbronko@gmail.com>
2022-09-16 18:32:41 +0300

    изучение и использование хуков

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, {useState} from "react";
-import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+
 
 function App() {
+	const [posts, setPosts] = useState([
+		{id: 1, title: 'Javascript', body: 'Description'},
+		{id: 2, title: 'Javascript 2', body: 'Description'},
+		{id: 3, title: 'Javascript 3', body: 'Description'},
 	
-	const [value, setValue] = useState('SURIC')
+	])
 	
+	
+	const [title, setTitle] = useState('')
+	const [body, setBody] = useState('')
+	
+	
+	const addNewPost = (e) => {
+		e.preventDefault()
+		const newPost = {
+			id: Date.now(),
+			title,
+			body
+		}
+		setPosts([...posts, newPost])
+		setTitle('')
+		setBody('')
+	}
 	
 	return (
 		<div className="App">
-			<ClassCounter/>
+			<form>
+				<MyInput
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+					type="text"
+					placeholder="Название поста"
+				/>
+				<MyInput
+					value={body}
+					onChange={e => setBody(e.target.value)}
+					type="text"
+					placeholder="Описание поста"
+				/>
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
+			</form>
+			<PostList posts={posts} title={"Посты про JS"}/>
 		</div>
+	
 	);
 }
 
